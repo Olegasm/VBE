2ab4ad812e67ea25062f1162d1b630aba55921b0
Olegasm <oleg.mirzianov@gmail.com>
2020-04-01 09:46:35 +0300

    Update __init__.py

--- 2020/__init__.py
+++ 2020/__init__.py
@@ -3,15 +3,20 @@ import check50.c
 
 @check50.check()
 def exists():
-    """hello.c exists."""
+    """hello.c exists"""
     check50.exists("hello.c")
 
 @check50.check(exists)
 def compiles():
-    """hello.c compiles."""
+    """hello.c compiles"""
     check50.c.compile("hello.c", lcs50=True)
 
 @check50.check(compiles)
-def veronica():
-    """responds to name Veronica."""
-    check50.run("./hello").stdin("Veronica").stdout("Veronica").exit()
+def emma():
+    """responds to name Emma"""
+    check50.run("./hello").stdin("Emma").stdout("Emma").exit()
+
+@check50.check(compiles)
+def rodrigo():
+    """responds to name Rodrigo"""
+    check50.run("./hello").stdin("Rodrigo").stdout("Rodrigo").exit()
