d36e984cf26d2f2260b93d8c4e99f66d1096f21a
Olegasm <oleg.mirzianov@gmail.com>
2020-04-02 14:11:11 +0300

    Update __init__.py

--- 2019/__init__.py
+++ 2019/__init__.py
@@ -13,7 +13,8 @@ def exists():
 @check50.check(exists)
 def compiles():
     """programa.cpp compiles."""
-    check50.c.compile("programa.cpp", cc="g++", lcrypt=True, lcs50=True, lm=True)
+    check50.c.compile("programa.cpp", cc="g++", lcrypt=True)
+    #Last working line: check50.c.compile("programa.cpp", cc="g++", lcrypt=True, lcs50=True, lm=True)
     #check50.c.compile("programa.cpp", cc="clang++", std="c++11", lcs50=True, Wall=True, Wextra=True, ggdb3=True, O0=True)    
     
 @check50.check(compiles)    
