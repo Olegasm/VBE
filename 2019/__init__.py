2824303cb4b58ca1bd6685d7cd217e32023012c2
Olegasm <oleg.mirzianov@gmail.com>
2020-04-02 14:44:26 +0300

    Update __init__.py

--- 2019/__init__.py
+++ 2019/__init__.py
@@ -11,7 +11,7 @@ def exists():
 @check50.check(exists)
 def compiles():
     """c.cpp compiles."""
-    check50.c.CC= 'g++'
+    #check50.c.CC= 'g++'
     #check50.c.CFLAGS= {'ggdb': True, 'lm': True, 'std': 'c++11'}
     check50.c.compile("c.cpp, cc="g++", std="c++11")
     # kaip kompiliuojamas C++ kodas sandbox'e: g++     c.cpp  -lcrypt -lcs50 -lm -o c
