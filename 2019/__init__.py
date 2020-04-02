ea5dd6b6ef6e9f18ac9b8c2bb72820bfb4759d9d
Olegasm <oleg.mirzianov@gmail.com>
2020-04-02 15:00:06 +0300

    Update __init__.py

--- 2019/__init__.py
+++ 2019/__init__.py
@@ -11,7 +11,7 @@ def exists():
 @check50.check(exists)
 def compiles():
     """c.cpp compiles."""
-    check50.c.compile("c.cpp", cc="g++", std="c++11")
+    check50.c.compile("c.cpp", cc="g++", std="c++11", lcrypt=True, lcs50=True, lm=True)
     # minimum: check50.c.compile("c.cpp", cc="g++", std="c++11")
     # kaip kompiliuojamas C++ kodas sandbox'e: g++     c.cpp  -lcrypt -lcs50 -lm -o c
     # Last working line: check50.c.compile("programa.cpp", cc="g++", lcrypt=True, lcs50=True, lm=True)
