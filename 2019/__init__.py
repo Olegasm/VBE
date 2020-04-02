476f52b87f50f36e04032ad54da29f909177e05f
Olegasm <oleg.mirzianov@gmail.com>
2020-04-02 14:51:36 +0300

    Update __init__.py

--- 2019/__init__.py
+++ 2019/__init__.py
@@ -1,6 +1,7 @@
 import check50
 import check50.c
 #import filecmp
+check50.c.CC= 'g++'
 
 @check50.check()
 def exists():
@@ -11,13 +12,13 @@ def exists():
 @check50.check(exists)
 def compiles():
     """c.cpp compiles."""
-    check50.c.CC= 'g++'
-    check50.c.CFLAGS= {'ggdb': True, 'lm': True, 'std': 'c++11'}
-    check50.c.compile("c.cpp")
-    # , cc="g++", std="c++11"
+    check50.c.compile("c.cpp", cc="g++", std="c++11")
+    # minimum: check50.c.compile("c.cpp", cc="g++", std="c++11")
     # kaip kompiliuojamas C++ kodas sandbox'e: g++     c.cpp  -lcrypt -lcs50 -lm -o c
-    #Last working line: check50.c.compile("programa.cpp", cc="g++", lcrypt=True, lcs50=True, lm=True)
-    #check50.c.compile("programa.cpp", cc="clang++", std="c++11", lcs50=True, Wall=True, Wextra=True, ggdb3=True, O0=True)    
+    # Last working line: check50.c.compile("programa.cpp", cc="g++", lcrypt=True, lcs50=True, lm=True)
+    # check50.c.compile("programa.cpp", cc="clang++", std="c++11", lcs50=True, Wall=True, Wextra=True, ggdb3=True, O0=True)    
+    # check50.c.CC= 'g++'
+    # check50.c.CFLAGS= {'ggdb': True, 'lm': True, 'std': 'c++11'}
     
 @check50.check(compiles)    
 def runCPP():
